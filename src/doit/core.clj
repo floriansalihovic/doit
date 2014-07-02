a2cd7477241f5f3bba164b24d2d435b6fe76f915
Florian Salihovic <florian.salihovic@me.com>
2014-07-02 12:48:09 +0200

    #5 Adding basic application structure to core.clj.

--- src/doit/core.clj
+++ src/doit/core.clj
@@ -1,6 +1,20 @@
-(ns doit.core)
+(ns doit.core
+  (:require [io.pedestal.http.route.definition :refer [defroutes]]
+            [io.pedestal.http.route as route :refer [router]]
+            [io.pedestal.http :as http]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn hello-world [req]
+  {:status 200
+   :body "Hello, world!"
+   :headers {}})
+
+(defroutes routes
+  [[["/" ["/hello" {:get hello-world}]]]])
+
+(def service {::http/interceptors [(router routes)]
+              ::http/port 8080})
+
+(defn -main [& args]
+  (-> service
+      http/create-server
+      http/start))
